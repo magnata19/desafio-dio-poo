eff004d3755337d88b7ac377acb83f56c2da9e27
Davidson <dehpacifico@gmail.com>
2024-01-19 22:44:09 -0300

    concluindo projeto "Abstraindo um bootcamp usando POO"

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,9 +22,33 @@ public class Main {
     mentoria.setDescricao("mentoria de java na dio");
     mentoria.setData(LocalDate.now());
 
-    System.out.println(curso);
-    System.out.println(curso1);
-    System.out.println(mentoria);
+//    System.out.println(curso);
+//    System.out.println(curso1);
+//    System.out.println(mentoria);
 
+    Bootcamp bootcamp = new Bootcamp();
+    bootcamp.setNome("Backend Java Developer");
+    bootcamp.setDescricao("Formação backend com ênfase em Java");
+    bootcamp.getConteudos().add(curso);
+    bootcamp.getConteudos().add(curso1);
+    bootcamp.getConteudos().add(mentoria);
+
+    Dev devDave = new Dev();
+    devDave.setNome("Davidson");
+    devDave.inscreverBootcamp(bootcamp);
+    devDave.progredir();
+    devDave.progredir();
+    devDave.progredir();
+    System.out.println("Conteúdos inscritos davidson: " + devDave.getConteudosInscritos());
+    System.out.println("Conteúdos concluidos davidson: " + devDave.getConteudosConcluidos());
+    System.out.println("XP: " + devDave.calcularTotalXp());
+
+    Dev devPacifico = new Dev();
+    devPacifico.setNome("Pacifico");
+    devPacifico.inscreverBootcamp(bootcamp);
+    devPacifico.progredir();
+    System.out.println("Conteúdos inscritos davidson: " + devPacifico.getConteudosInscritos());
+    System.out.println("Conteúdos concluidos davidson: " + devPacifico.getConteudosConcluidos());
+    System.out.println("XP: " + devPacifico.calcularTotalXp());
   }
 }
